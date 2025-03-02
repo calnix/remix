9aa573b97f6e5c638ef8470be0da919bf12f70ac
calnix <calnix.289@gmail.com>
2025-03-02 19:36:36 +0700

    update

--- testCasting.sol
+++ testCasting.sol
@@ -5,11 +5,12 @@ contract RouterOnPool {
 
     uint public num;
 
-    function cast() public view returns(uint128) {
+    function cast() public view returns(bytes32) {
         
         //num = type(uint256).max;
+        address WHITELISTED_ADDRESS = 0x228e0f99adf8bf367100BBFD9b090Bc0b15c89a9;
 
-        return uint128(type(uint256).max);
+        return bytes32(uint256(uint160(address(WHITELISTED_ADDRESS))));
     }
 
 }
